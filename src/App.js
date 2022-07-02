14b84a541fe9c5df482f3994731f4b405b2d0987
Stella Raab <hallo@stellaraab.de>
2022-07-02 14:52:06 +0200

    optimized styling, added images

--- src/App.js
+++ src/App.js
@@ -49,14 +49,15 @@ const App = () => {
                 <div className="head-image-II"></div>
                 <div className="head-image-III"></div>
               </div>
+              <div className="head-bg-image"></div>
               <div className="headlines styled-content">
                 <h1>Stella's Portfolio</h1>
                 <h2>EST <span className="varyear"></span></h2>
-                <p className="headp">It's the year <span className="varyear"></span>. My name is Stella Raab, and I’m a passionate Graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself).</p>
+                <p className="headp">It's the year <span className="varyear"></span>. My name is Stella Raab, and I’m a passionate graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself).</p>
               </div>
             </div>
-            <p className="styled-content intro">It's the year <span className="varyear"></span>. My name is Stella Raab, and I’m a passionate Graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself).</p>
-            <div className="intro-2017"><p>It's the year 2017. My name is Stella Raab, and I’m a passionate Graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself)</p>
+            <p className="styled-content intro">It's the year <span className="varyear"></span>. My name is Stella Raab, and I’m a passionate graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself).</p>
+            <div className="intro-2017"><p>It's the year 2017. My name is Stella Raab, and I’m a passionate graphic-designer, based in Berlin, Germany. I draw and eat things. As often as possible I love to go to the gym, to throw around heavy things (including myself)</p>
            </div>
             <div className="boxes">
               <div className="content-box white-grad">
