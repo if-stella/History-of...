ab03fbf19e8d553c83d2369e2057598451f68710
Stella Raab <hallo@stellaraab.de>
2022-07-07 21:02:49 +0200

    Created components, added guest-counter

---
+++ src/components/Interfacebar/Interfacebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Interfacebar.scss'
+
+const Interfacebar = () => {
+  return (
+    <div className="interface-bar">
+      <div className="ibar-image"></div>
+      I'm an in-your-face-interface!
+    </div>
+  )
+}
+
+export default Interfacebar
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <li>
+          <ul>Anchor</ul>
+          <ul>Button</ul>
+          <ul>Something</ul>
+        </li>
+    </div>
+  )
+}
+
+export default Navbar
