8f91024ed08e7beba7ce42f7added1c86b389826
Stella Raab <hallo@stellaraab.de>
2022-07-10 13:11:09 +0200

    Visibility sensor is now implemented again

--- src/components/Guestcounter/Guestcounter.jsx
+++ src/components/Guestcounter/Guestcounter.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 import './Guestcounter.scss'
 
 const Guestcounter = () => {
   return (
     <div className="guestCounter">
       <span>
-      <CountUp end={1337} duration={9} prefix="You are visitor number " suffix="!!!"/>
+      <CountUp end={1337} duration={9} prefix="You are visitor number " suffix="!!!">
+        {({ countUpRef, start }) => (
+            <VisibilitySensor onChange={start} delayedCall>
+                <span ref={countUpRef} />
+            </VisibilitySensor>
+        )}
+      </CountUp>
       </span>
     </div>
   )
